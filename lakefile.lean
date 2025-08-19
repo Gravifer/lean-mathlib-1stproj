d6141d5078136707d0c1b781381ed4176107eb0f
Gravifer <44160838+Gravifer@users.noreply.github.com>
2025-08-19 20:41:03 +0800

    deps: use lean lakefile instead of toml

---
+++ lakefile.lean
@@ -0,0 +1,14 @@
+import Lake
+
+open System Lake DSL
+
+package «lean-matlib-1stproj» where
+  version := v!"0.1.0"
+  keywords := #["math"]
+  leanOptions :=
+  #[⟨`pp.unicode.fun, true⟩, ⟨`autoImplicit, false⟩, ⟨`relaxedAutoImplicit, false⟩,
+    ⟨`weak.linter.mathlibStandardSet, true⟩, ⟨`maxSynthPendingDepth, 3⟩]
+
+require "leanprover-community" / mathlib
+
+@[default_target] lean_lib LeanMatlib1stproj
