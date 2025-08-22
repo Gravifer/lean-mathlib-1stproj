b517ee72504894ce956cbdc4b5301097b1640e34
Gravifer <44160838+Gravifer@users.noreply.github.com>
2025-08-22 11:16:25 +0800

    read(fpil): finish §2.1

---
+++ LeanMathlib1stproj/fpil/cpt2/HelloName.lean
@@ -0,0 +1,13 @@
+-- # Combining IO Actions
+
+def main : IO Unit := do -- see Haskell do syntax
+  let stdin  ← IO.getStdin
+  let stdout ← IO.getStdout
+
+  stdout.putStrLn "How would you like to be addressed?"
+  let input ← stdin.getLine
+  let name := input.dropRightWhile Char.isWhitespace
+
+  stdout.putStrLn s!"Hello, {name}!"
+
+--
