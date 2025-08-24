44c778ef3226f05fd28d3b88ef14bf2a9a74a59f
Gravifer <44160838+Gravifer@users.noreply.github.com>
2025-08-24 19:38:01 +0800

    read(fpil): finish §5.4 - IO monad

---
+++ LeanMathlib1stproj/fpil/cpt5/5-4_IO-monad.lean
@@ -0,0 +1,24 @@
+set_option autoImplicit true
+set_option eval.pp true
+set_option pp.all true
+
+#print Nat
+
+#print Char.isAlpha
+
+#print List.isEmpty
+#check @List.isEmpty.{0}
+
+#print StateM
+#print StateT
+
+#print IO
+#print IO.RealWorld
+#print IO.Error
+
+#print EIO
+#print EStateM
+#print EStateM.Result
+#print EStateM.bind
+
+--
