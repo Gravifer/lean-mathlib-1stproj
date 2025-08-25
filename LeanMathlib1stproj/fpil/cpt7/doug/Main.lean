e6313d3b502feb6b47e095c18c868868f9f458bf
Gravifer <44160838+Gravifer@users.noreply.github.com>
2025-08-26 06:30:57 +0800

    read(fpil): §7.1 exercise - `doug` - start path arg handling

--- LeanMathlib1stproj/fpil/cpt7/doug/Main.lean
+++ LeanMathlib1stproj/fpil/cpt7/doug/Main.lean
@@ -33,19 +33,46 @@ partial def dirTree (path : System.FilePath) : ConfigIO Unit := do
                           (doList contents.toList fun d =>
                             dirTree d.path)
 
+def DirFromArgs (args : List String) : Except Unit (Option System.FilePath) := do
+  /- - if there is not the `--` separator,
+       then the first argument after flags (should it exist)
+       is the directory path and cannot start with '-' (cwd if none given);
+     - if the `--` separator exists, the rest of the arguments should be joined
+       as a space separated string, and regarded as the path (error if none given).
+  -/
+  if args.contains "--" then
+    let afterSeparator := (args.dropWhile (· != "--")).drop 1
+    if afterSeparator.isEmpty then
+      throw ()  -- error if no path given after --
+    let pathStr := " ".intercalate afterSeparator
+    let path := System.FilePath.mk pathStr
+    return (some path)
+  else
+    return (args.dropWhile (·.startsWith "-")).head?.map System.FilePath.mk
+
 end Doug
 
 open Doug in
 def main (args : List String) : IO UInt32 := do
   let cwd ← IO.currentDir
-  match configFromArgs args with
-  | some config =>
-      match config with
-      | {printHelp := true} => IO.println usage
-      | _ => (dirTree cwd).run config
-      pure 0
-  | none =>
-      IO.eprintln s!"Didn't understand argument(s) {" ".intercalate args}\n"
+  match DirFromArgs args with
+  | Except.ok argDir =>
+      IO.eprintln s!"Debug: passed path argument is {argDir}"
+      -- todo: check if directory exists, and whether it's relative
+      let startDir := argDir.getD cwd
+      IO.eprintln s!"Debug: start directory is {startDir}"
+      match configFromArgs (args.takeWhile fun str => str.startsWith "-" && str != "--") with
+      | some config =>
+          match config with
+          | {printHelp := true} => IO.println usage
+          | _ => (dirTree startDir).run config
+          pure 0
+      | none =>
+          IO.eprintln s!"Didn't understand argument(s) {" ".intercalate args}\n"
+          IO.eprintln usage
+          pure 1
+  | Except.error _ =>
+      IO.eprintln "Error: No directory path provided after '--'"
       IO.eprintln usage
       pure 1
 
