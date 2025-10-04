73c6af4de913e160d75cd679998804af444f2a28
Gravifer <44160838+Gravifer@users.noreply.github.com>
2025-10-04 15:54:53 +0800

    read(mpil): finish 1. Introduction

--- LeanMathlib1stproj/mpil/Main/Introduction.lean
+++ LeanMathlib1stproj/mpil/Main/Introduction.lean
@@ -1,1 +1,93 @@
+import Lean
+/-! # Introduction -/
 namespace mpil
+
+/-! ## Metaprogramming examples -/
+
+--! Introducing notation (defining new syntax)
+macro x:ident ":" t:term " ↦ " y:term : term => do
+  `(fun $x : $t => $y)
+
+#eval (x : Nat ↦ x + 2) 2 -- 4
+
+macro x:ident " ↦ " y:term : term => do
+  `(fun $x  => $y)
+
+#eval (x ↦  x + 2) 2 -- 4
+
+--! Building a command
+elab "#assertType " termStx:term " : " typeStx:term : command =>
+  open Lean Lean.Elab Command Term in
+  liftTermElabM
+    try
+      let tp ← elabType typeStx
+      discard $ elabTermEnsuringType termStx tp
+      synthesizeSyntheticMVarsNoPostponing
+      logInfo "success"
+    catch | _ => throwError "failure"
+
+#assertType 5 : Nat
+
+-- don't display names of metavariables
+set_option pp.mvars false in
+#assertType [] : Nat
+
+--! Building a DSL and a syntax for it
+inductive Arith : Type where
+  | add : Arith → Arith → Arith -- e + f
+  | mul : Arith → Arith → Arith -- e * f
+  | nat : Nat → Arith           -- constant
+  | var : String → Arith        -- variable
+
+declare_syntax_cat arith
+syntax num                        : arith -- nat for Arith.nat
+syntax str                        : arith -- strings for Arith.var
+syntax:50 arith:50 " + " arith:51 : arith -- Arith.add
+syntax:60 arith:60 " * " arith:61 : arith -- Arith.mul
+syntax " ( " arith " ) "          : arith -- bracketed expressions
+
+-- Auxiliary notation for translating `arith` into `term`
+syntax " ⟪ " arith " ⟫ " : term
+
+-- Our macro rules perform the "obvious" translation:
+macro_rules
+  | `(⟪ $s:str ⟫)              => `(Arith.var $s)
+  | `(⟪ $num:num ⟫)            => `(Arith.nat $num)
+  | `(⟪ $x:arith + $y:arith ⟫) => `(Arith.add ⟪ $x ⟫ ⟪ $y ⟫)
+  | `(⟪ $x:arith * $y:arith ⟫) => `(Arith.mul ⟪ $x ⟫ ⟪ $y ⟫)
+  | `(⟪ ( $x ) ⟫)              => `( ⟪ $x ⟫ )
+
+#check ⟪ "x" * "y" ⟫
+-- Arith.mul (Arith.var "x") (Arith.var "y") : Arith
+
+#check ⟪ "x" + "y" ⟫
+-- Arith.add (Arith.var "x") (Arith.var "y") : Arith
+
+#check ⟪ "x" + 20 ⟫
+-- Arith.add (Arith.var "x") (Arith.nat 20) : Arith
+
+#check ⟪ "x" + "y" * "z" ⟫ -- precedence
+-- Arith.add (Arith.var "x") (Arith.mul (Arith.var "y") (Arith.var "z")) : Arith
+
+#check ⟪ "x" * "y" + "z" ⟫ -- precedence
+-- Arith.add (Arith.mul (Arith.var "x") (Arith.var "y")) (Arith.var "z") : Arith
+
+#check ⟪ ("x" + "y") * "z" ⟫ -- brackets
+-- Arith.mul (Arith.add (Arith.var "x") (Arith.var "y")) (Arith.var "z")
+
+--! Writing our own tactic
+open Lean Meta Elab Tactic Term in
+elab "suppose " n:ident " : " t:term : tactic => do
+  let n : Name := n.getId
+  let mvarId ← getMainGoal
+  mvarId.withContext do
+    let t ← elabType t
+    let p ← mkFreshExprMVar t MetavarKind.syntheticOpaque n
+    let (_, mvarIdNew) ← MVarId.intro1P $ ← mvarId.assert n t p
+    replaceMainGoal [p.mvarId!, mvarIdNew]
+  evalTactic $ ← `(tactic|rotate_left)
+set_option autoImplicit true in
+example : 0 + a = a := by
+  suppose add_comm : 0 + a = a + 0
+  rw [add_comm]; rfl     -- closes the initial main goal
+  rw [Nat.zero_add]; rfl -- proves `add_comm`
