f48236ce97b4fcba51f1e6256715644bcda293b5
Gravifer <44160838+Gravifer@users.noreply.github.com>
2025-10-07 22:27:11 +0800

    read(mpil): through 4. MetaM: against delayed assignments

---
+++ LeanMathlib1stproj/mpil/main/04_metam.lean
@@ -0,0 +1,109 @@
+import Lean
+import Batteries
+/-! # `MetaM` -/
+/- * Lean 4 metaprogramming monad zoo
+- `CoreM` gives access to the *environment*, i.e. the set of things that
+  have been declared or imported at the current point in the program.
+- `MetaM` gives access to the *metavariable context*, i.e. the set of
+  metavariables that are currently declared and the values assigned to them (if
+  any).
+- `TermElabM` gives access to various information used during elaboration.
+- `TacticM` gives access to the list of current goals.
+
+- `CommandElabM` extends `MetaM` but is incomparable with `TermElabM`.
+-/
+open Lean Lean.Expr Lean.Meta
+
+/-! ## Metavariables -/
+/-! ### Tactic Communication via Metavariables -/
+/-! ### Basic Operations -/
+#print Lean.Meta.mkFreshExprMVar
+#print Lean.Meta.mkFreshExprMVarAt
+#print Lean.MVarId.assign
+#print Lean.MVarId.getDecl
+#print Lean.MVarId.getType
+#print Lean.getExprMVarAssignment?
+#print Lean.MVarId.isAssigned
+#print Lean.instantiateMVars
+#print Lean.MVarId.instantiateMVars -- in Batteries
+
+#eval show MetaM Unit from do
+  -- Create two fresh metavariables of type `Nat`.
+  let mvar1 ← mkFreshExprMVar (Expr.const ``Nat []) (userName := `mvar1)
+  let mvar2 ← mkFreshExprMVar (Expr.const ``Nat []) (userName := `mvar2)
+  -- Create a fresh metavariable of type `Nat → Nat`. The `mkArrow` function
+  -- creates a function type.
+  let mvar3 ← mkFreshExprMVar (← mkArrow (.const ``Nat []) (.const ``Nat []))
+    (userName := `mvar3)
+
+  -- Define a helper function that prints each metavariable.
+  let printMVars : MetaM Unit := do
+    IO.println s!"  meta1: {← instantiateMVars mvar1}"
+    IO.println s!"  meta2: {← instantiateMVars mvar2}"
+    IO.println s!"  meta3: {← instantiateMVars mvar3}"
+
+  IO.println "Initially, all metavariables are unassigned:"
+  printMVars
+
+  -- Assign `mvar1 : Nat := ?mvar3 ?mvar2`.
+  mvar1.mvarId!.assign (.app mvar3 mvar2)
+  IO.println "After assigning mvar1:"
+  printMVars
+
+  -- Assign `mvar2 : Nat := 0`.
+  mvar2.mvarId!.assign (.const ``Nat.zero [])
+  IO.println "After assigning mvar2:"
+  printMVars
+
+  -- Assign `mvar3 : Nat → Nat := Nat.succ`.
+  mvar3.mvarId!.assign (.const ``Nat.succ [])
+  IO.println "After assigning mvar3:"
+  printMVars
+-- Initially, all metavariables are unassigned:
+--   meta1: ?_uniq.1
+--   meta2: ?_uniq.2
+--   meta3: ?_uniq.3
+-- After assigning mvar1:
+--   meta1: ?_uniq.3 ?_uniq.2
+--   meta2: ?_uniq.2
+--   meta3: ?_uniq.3
+-- After assigning mvar2:
+--   meta1: ?_uniq.3 Nat.zero
+--   meta2: Nat.zero
+--   meta3: ?_uniq.3
+-- After assigning mvar3:
+--   meta1: Nat.succ Nat.zero
+--   meta2: Nat.zero
+--   meta3: Nat.succ
+
+/-! ### Local Contexts -/
+#print Lean.getLCtx
+#print Lean.MVarId.withContext
+#print Lean.FVarId.getDecl
+#print Lean.Meta.getLocalDeclFromUserName
+#print ForIn
+
+def myAssumption (mvarId : MVarId) : MetaM Bool := do
+  -- Check that `mvarId` is not already assigned.
+  mvarId.checkNotAssigned `myAssumption
+  -- Use the local context of `mvarId`.
+  mvarId.withContext do
+    -- The target is the type of `mvarId`.
+    let target ← mvarId.getType
+    -- For each hypothesis in the local context:
+    for ldecl in ← getLCtx do
+      -- If the hypothesis is an implementation detail, skip it.
+      if ldecl.isImplementationDetail then
+        continue
+      -- If the type of the hypothesis is definitionally equal to the target
+      -- type:
+      if ← isDefEq ldecl.type target then
+        -- Use the local hypothesis to prove the goal.
+        mvarId.assign ldecl.toExpr
+        -- Stop and return true.
+        return true
+    -- If we have not found any suitable local hypothesis, return false.
+    return false
+#print Lean.MVarId.checkNotAssigned
+#print Lean.Meta.isDefEq
+#print Lean.LocalDecl.toExpr
