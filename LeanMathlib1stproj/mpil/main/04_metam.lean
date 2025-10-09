06ae199959b4bb782ec924677d631199708b345e
Gravifer <44160838+Gravifer@users.noreply.github.com>
2025-10-09 14:26:42 +0800

    read(mpil): through 4. MetaM: deconstructing Expressions

--- LeanMathlib1stproj/mpil/main/04_metam.lean
+++ LeanMathlib1stproj/mpil/main/04_metam.lean
@@ -112,3 +112,114 @@ def myAssumption (mvarId : MVarId) : MetaM Bool := do
 #print Lean.Meta.withNewMCtxDepth
 
 /-! ## Computation -/
+/-! ### Full Normalisation -/
+#print Lean.Meta.reduce
+#print Lean.Expr
+#print Lean.Literal
+/- Optional arguments of reduce allow us to skip certain parts of an expression.
+  E.g. `reduce e (explicitOnly := true)` does not normalise any implicit arguments in the expression `e`.
+  This yields better performance. -/
+#reduce (· + 2) $ 3
+
+/-! ### Transparency -/
+/- An ugly but important detail of Lean 4 metaprogramming is that
+  any given expression does not have a single normal form.
+  Rather, it has a normal form up to a given *transparency*. -/
+#print Lean.Meta.TransparencyMode
+#print Lean.Meta.getTransparency
+/- The four settings unfold progressively more constants:
+  - `reducible`: unfold only constants tagged with the `@[reducible]` attribute.
+      Note that `abbrev` is a shorthand for `@[reducible] def`.
+  - `instances`: unfold reducible constants and constants tagged with the `@[instance]` attribute.
+      Again, the `instance` command is a shorthand for `@[instance] def`.
+  - `default`: unfold all constants except those tagged as `@[irreducible]`.
+  - `all`: unfold all constants, even those tagged as `@[irreducible]`. -/
+#print Lean.Meta.withTransparency
+#print Lean.Meta.withReducible
+#print Lean.Meta.ppExpr
+def traceConstWithTransparency (md : TransparencyMode) (c : Name) :
+    MetaM Format := do
+  ppExpr (← withTransparency md $ reduce (.const c []))
+
+@[irreducible] def irreducibleDef : Nat      := 1
+def                defaultDef     : Nat      := irreducibleDef + 1
+abbrev             reducibleDef   : Nat      := defaultDef + 1
+#eval traceConstWithTransparency .reducible ``reducibleDef
+set_option pp.explicit true in
+#eval traceConstWithTransparency .reducible ``reducibleDef
+#eval traceConstWithTransparency .instances ``reducibleDef
+#eval traceConstWithTransparency .default   ``reducibleDef
+#eval traceConstWithTransparency .all       ``reducibleDef
+
+/-! ### Weak Head Normalisation -/
+#print whnf
+#print Lean.Meta.whnfImp
+open Lean.Elab.Term in
+def whnf' (e : TermElabM Syntax) : TermElabM Format := do
+  let e ← elabTermAndSynthesize (← e) none
+  ppExpr (← whnf e)
+#eval whnf' `(List.cons 1 [])
+#eval whnf' `(List.cons (1 + 1) [])
+#eval withTransparency .reducible $ whnf' `(List.append [1] [2])
+#eval whnf' `(λ x : Nat => x)
+#eval whnf' `(∀ x, x > 0)
+#eval whnf' `(Type 3)
+#eval whnf' `((15 : Nat))
+/- Some more expressions in WHNF which are a bit tricky to test:
+    ```lean
+      ?x 0 1  -- Assuming the metavariable `?x` is unassigned, it is in WHNF.
+      h  0 1  -- Assuming `h` is a local hypothesis, it is in WHNF.
+    ```
+  Some tricky examples *not* in WHNF:
+    ```lean
+      ?x 0 1 -- Assuming `?x` is assigned (e.g. to `Nat.add`),
+                  its application is not in WHNF.
+      h 0 1  -- Assuming `h` is a local definition (e.g. with value `Nat.add`),
+                  its application is not in WHNF.
+    ```
+-/
+def matchAndReducing (e : Expr) : MetaM (Option (Expr × Expr)) := do
+  match ← whnf e with
+  | (.app (.app (.const ``And _) P) Q) => return some (P, Q)
+  | _ => return none
+def matchAndReducing₂ (e : Expr) : MetaM (Option (Expr × Expr × Expr)) := do
+  match ← whnf e with
+  | (.app (.app (.const ``And _) P) e') =>
+    match ← whnf e' with
+    | (.app (.app (.const ``And _) Q) R) => return some (P, Q, R)
+    | _ => return none
+  | _ => return none
+
+/-! ### Definitional Equality -/
+#print Lean.Meta.isDefEq
+#print Lean.Meta.isExprDefEq
+#print MetavarKind
+
+/-! ## Constructing Expressions -/
+/-! ### Applications -/
+set_option autoImplicit true in
+def appendAppend (xs ys : List α) := (xs.append ys).append xs
+set_option pp.all true in
+set_option pp.explicit true in
+#print appendAppend
+
+#print Lean.Meta.mkAppM --
+#print Lean.Meta.mkAppM'--
+#print Lean.Meta.mkAppOptM
+#print Lean.Meta.mkAppOptM'
+
+/-! ### Lambdas and Foralls -/
+/- Instead of using `bvar`s directly, the Lean way
+    is to construct expressions with bound variables in two steps:
+1. Construct the body of the expression (in our example: the body of the lambda),
+    using temporary local hypotheses (`fvar`s) to stand in for the bound variables.
+2. Replace these `fvar`s with `bvar`s and, at the same time,
+    add the corresponding lambda binders.
+-/
+#print Lean.Meta.withLocalDecl
+#print Lean.Meta.mkLambdaFVars
+#print Lean.Meta.mkForallFVars
+#print Lean.Meta.mkForallFVars'
+#print Lean.mkArrow
+
+/-! ### Deconstructing Expressions -/
